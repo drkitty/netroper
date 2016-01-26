4c2c4c968e2c11bc76154a5598cdbfc5122b5b9c
drkitty <bumblingkitty@gmail.com>
2016-01-25 22:21:30 -0800

    Add some initial files

---
+++ Makefile
@@ -0,0 +1,38 @@
+MAKEFLAGS += --no-builtin-rules
+.SUFFIXES:
+
+.SECONDEXPANSION:
+
+
+EXE_SRC := netroper.c
+SRC := $(EXE_SRC) fail.c
+
+OBJ := $(SRC:%.c=%.o)
+EXE := $(EXE_SRC:%.c=%)
+
+CC := gcc
+CFLAGS := -std=c99 -pedantic -g -Wall -Wextra -Werror -Wno-unused-function
+
+
+all: $(EXE) $(EXTRA_EXE)
+
+$(OBJ): $$(patsubst %.o,%.c,$$@)
+	$(CC) $(CFLAGS) -c -o $@ $<
+
+$(EXE) $(EXTRA_EXE):
+	$(CC) -o $@ $^
+
+$(EXE): $$@.o
+
+clean:
+	rm -f $(OBJ) $(EXE)
+
+
+fail.o: common.h fail.h
+netroper.o: common.h fail.h
+
+netroper: fail.o
+
+
+.DEFAULT_GOAL := all
+.PHONY: all clean
---
+++ fail.h
@@ -0,0 +1,31 @@
+#pragma once
+
+
+#include "common.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#define v0(...) do { vx_(__FILE__, __LINE__, __VA_ARGS__); } while (0)
+#define v1(...) do { if (verbosity >= 1) vx_(__FILE__, __LINE__, \
+                __VA_ARGS__); } while (0)
+#define v2(...) do { if (verbosity >= 2) vx_(__FILE__, __LINE__, \
+                __VA_ARGS__); } while (0)
+
+#define warning(...) do { warning_(__FILE__, __LINE__, __VA_ARGS__); } \
+                     while (0)
+#define warning_e(...) do { warning_e_(__FILE__, __LINE__, __VA_ARGS__); } \
+                       while (0)
+#define fatal(rtn, ...) do { fatal_(rtn, __FILE__, __LINE__, \
+                        __VA_ARGS__); } while (0)
+#define fatal_e(rtn, ...) do { fatal_e_(rtn, __FILE__, __LINE__, \
+                          __VA_ARGS__); } while (0)
+
+
+void vx_(const char* srcname, int line, const char* format, ...);
+void warning_(const char* srcname, int line, const char* format, ...);
+void warning_e_(const char* srcname, int line, const char* format, ...);
+void fatal_(int rtn, const char* srcname, int line, const char* format, ...);
+void fatal_e_(int rtn, const char* srcname, int line,
+    const char* format, ...);
